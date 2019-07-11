f695bf488c414c8b2aa1a62c85d7668616357483
jonbore <zhouboIT@163.com>
2019-07-11 17:13:52 +0800

    keytool add crt

--- src/main/java/com/jonbore/Util/HttpsKeyDemo.java
+++ src/main/java/com/jonbore/Util/HttpsKeyDemo.java
@@ -21,6 +21,7 @@ import javax.swing.event.ListSelectionEvent;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.util.LinkedList;
@@ -32,22 +33,22 @@ import java.security.KeyStore;
 
 public class HttpsKeyDemo {
 	// 客户端证书路径，用了本地绝对路径，需要修改
-	private final static String PFX_PATHAPI = "C:/Users/bo.zhou1.XXX/Documents/api证书20190709/api_client01.p12";
+	private final static String PFX_PATHAPI = "C:/Users/bo.zhou1.XXXX/Documents/api证书20190709/api_client01.p12";
 	private final static String PFX_PATHAPI0 = "C:/Users/bo.zhou1.XXX/Documents/api证书20190709/api0_client.p12";
     private final static String PFX_PWDAPI0 = "*****"; //客户端证书密码及密钥库密码
-    private final static String PFX_PWDAPI = "**********";
+    private final static String PFX_PWDAPI = "****@#";
 
     public static String sslRequestGet(String url) throws Exception {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        InputStream instream = new FileInputStream(new File(PFX_PATHAPI0));
+        InputStream instream = new FileInputStream(new File(PFX_PATHAPI));
         try {
             // 这里就指的是KeyStore库的密码
-            keyStore.load(instream, PFX_PWDAPI0.toCharArray());
+            keyStore.load(instream, PFX_PWDAPI.toCharArray());
         } finally {
             instream.close();
         }
 
-        SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, PFX_PWDAPI0.toCharArray()).build();
+        SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, PFX_PWDAPI.toCharArray()).build();
         SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext
                 , new String[] { "TLSv1" }  // supportedProtocols ,这里可以按需要设置
                 , null  // supportedCipherSuites
@@ -59,6 +60,7 @@ public class HttpsKeyDemo {
             HttpPost httpPost = new HttpPost(url);
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("pwd", "123456");
+            jsonObject.put("code", "959607B0F119");
             jsonObject.put("username", "test");
             StringEntity stringEntity = new StringEntity(jsonObject.toString());
             httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
@@ -77,10 +79,22 @@ public class HttpsKeyDemo {
             httpclient.close();
         }
     }
+    
+    public static void importCrt(String keytool,String keyStore, String path, String keyPass, String aliasName) {
+    	//"/bin/sh","-c" linux
+		String[] cmd = {"cmd","/c",keytool+" -import -keystore "+keyStore+"  -storepass changeit -keypass "+keyPass+" -alias "+aliasName+" -file "+path+" -noprompt"};
+		try {
+			Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
     public static void main(String[] args) throws Exception {
-        System.out.println(sslRequestGet("https://api0.XXX.com/rest/console/user/login"));
+        System.out.println(sslRequestGet("https://api.XXXX.com/rest/servicepublish/service/temp/System01/menu/post"));
 //        System.out.println(sslRequestGet("https://api.XXX.com"));
+//        importCrt("keytool", "\"D:/env/Java/jdk1.8.0_201/jre/lib/security/cacerts\"", "C:/Users/bo.zhou1.XXXX/Documents/api证书20190709/rootca.crt", "\"******&*\"", "api0001.XXXX.com");
     }
 
 }
